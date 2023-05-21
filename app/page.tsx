1d1ac4e56a4efb561d622822059e19be04c6c9c1
SjoerdGourley <private@sjoerdgourley.com>
2023-05-22 00:42:59 +0300

    PrismaClientInitiolizationError Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import getCurrentUser from "./actions/getcurrentuser";
 import getListings, { IListingsParams } from "./actions/getlistings";
 import ClientOnly from "./components/clientonly";
